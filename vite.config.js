bf76eb15db6f9d00e6d0601ce7018c5f89697d0b
keisuke sato <corydoras121@gmail.com>
2023-05-27 22:52:43 +0900

    first

---
+++ vite.config.js
@@ -0,0 +1,42 @@
+import { defineConfig } from 'vite';
+
+//import設定を追記
+import { resolve } from 'path';
+
+
+export default defineConfig({
+  base: "/base_url/", //ベースとなるパブリックパス
+  root: "./src", //開発ディレクトリ設定 index.htmlが置かれている場所
+  // publicDir: resolve(__dirname, 'src/public'),
+  // publicDir: "./src/public",
+  build: {
+		outDir: "../dist", //出力場所の指定
+    rollupOptions: { //ファイル出力設定
+      output: {
+        assetFileNames: (assetInfo) => {
+          let extType = assetInfo.name.split('.')[1];
+
+          // if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+          //   extType = 'images';
+          // }
+
+          //ビルド時のCSS名を明記してコントロールする
+          if(extType === 'css') {
+            return `assets/css/style.css`;
+          }
+          return `assets/${extType}/[name][extname]`;
+        },
+        chunkFileNames: 'assets/js/[name].js',
+        // chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name].js',
+        // chunkFileNames: 'assets/js/bundle.js',
+        // entryFileNames: 'assets/js/bundle.js',
+      },
+      //生成オブジェクトを渡す
+      input:{
+        index: resolve(__dirname, './src/index.html'),
+        // about: resolve(__dirname, './src/about/index.html')
+      }
+    },
+	},
+});
