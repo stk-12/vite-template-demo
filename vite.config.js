959abfb16a9856f37924264c09ad42ec5427279b
keisuke sato <corydoras121@gmail.com>
2023-09-22 00:09:43 +0900

    multi page

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,64 @@ import autoprefixer from "autoprefixer";
 import imageminPlugin from '@macropygia/vite-plugin-imagemin-cache'
 
 // import設定を追記
-// import { resolve } from 'path';
+import { resolve } from 'path';
 
+// HTMLの複数出力を自動化する
+//./src配下のファイル一式を取得
+import fs from 'fs';
+import path from 'path';
+
+const files = [];
+function readDirectory(dirPath) {
+  const items = fs.readdirSync(dirPath);
+
+  for (const item of items) {
+    const itemPath = path.join(dirPath, item);
+
+    if (fs.statSync(itemPath).isDirectory()) {
+      // componentsディレクトリを除外する
+      if (item === 'components') {
+        continue;
+      }
+
+      readDirectory(itemPath);
+    } else {
+      // htmlファイル以外を除外する
+      if (path.extname(itemPath) !== '.html') {
+        continue;
+      }
+
+      // nameを決定する
+      let name;
+      if (dirPath === path.resolve(__dirname, 'src')) {
+        name = path.parse(itemPath).name;
+      } else {
+        const relativePath = path.relative(path.resolve(__dirname, 'src'), dirPath);
+        const dirName = relativePath.replace(/\//g, '_');
+        name = `${dirName}_${path.parse(itemPath).name}`;
+      }
+
+      // pathを決定する
+      const relativePath = path.relative(path.resolve(__dirname, 'src'), itemPath);
+      const filePath = `/${relativePath}`;
+
+      files.push({ name, path: filePath });
+    }
+  }
+}
+readDirectory(path.resolve(__dirname, 'src'));
+const inputFiles = {};
+for (let i = 0; i < files.length; i++) {
+  const file = files[i];
+  inputFiles[file.name] = resolve(__dirname, './src' + file.path );
+}
+/*
+  この形を自動的に作る
+  input:{
+    index: resolve(__dirname, './src/index.html'),
+    list: resolve(__dirname, './src/list.html')
+  }
+*/
 
 export default defineConfig({
   // base: "/base_url/", //ベースパス（ルートパス）
@@ -37,11 +93,8 @@ export default defineConfig({
         // chunkFileNames: 'assets/js/[name]-[hash].js',
       },
       //生成オブジェクトを渡す
-      input:{
-        main: './src/index.html',
-        // index: resolve(__dirname, './src/index.html'),
-        // about: resolve(__dirname, './src/about/index.html')
-      }
+      // 
+      input: inputFiles,
     },
 	},
   css: {
